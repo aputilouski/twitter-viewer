8b5d46a6ac50c10494719223ff2e6cabd822506a
Andrei Putilouski <andrey.ati.97@gmail.com>
2021-07-08 18:19:20 +0300

    fix login request

--- frontend/src/api/authorizationApi.js
+++ frontend/src/api/authorizationApi.js
@@ -1,9 +1,9 @@
-import instance, {defaultInstance} from "./_instance";
+import {defaultInstance} from "./_instance";
 
 
 const AuthorizationAPI = {
     getTwitterAuthorizePage() {
-        return instance().get(`auth/request_token/`)
+        return defaultInstance.get(`auth/request_token/`)
     },
     refreshToken() {
         return defaultInstance.post(`auth/refresh_token/`, null, {
