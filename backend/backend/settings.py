b628524b599867894fdfb9e7cb8ddaa572d90e1e
Andrei Putilouski <work.aputilouski@gmail.com>
2021-07-11 18:15:35 +0300

    renaming dir of twitter viewer app:

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app',
+    'twitter_viewer',
     'authorization',
     'corsheaders',
     'rest_framework',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from .views import index
 urlpatterns = [
     path('', index),
     path('admin/', admin.site.urls),
-    path('api/', include('main_app.urls')),
+    path('api/', include('twitter_viewer.urls')),
     path('api/auth/', include('authorization.urls')),
     path('main', index),
     path('about', index),
